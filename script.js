05d1011c212b931159e1768ef7b33e39951bf70c
NATAN BRENO FERREIRA <natan_b_ferreira@estudante.sesisenai.org.br>
2025-04-24 09:03:54 -0300

    adicionar métodos

--- script.js
+++ script.js
@@ -12,11 +12,19 @@ class mago extends Personagem{
     constructor(mana, vida, forca){
         super(mana, vida, forca)
     }
+    Usarmagia(){
+        console.log("usando magia")
+    
+    }
+    
 }
 
 class guerreiro extends Personagem{
     constructor(mana, forca, vida){
         super (mana, vida, forca)
+    }
+    defender(){
+        console.log ("defendendo")
     }
 }
 
@@ -24,4 +32,8 @@ class arqueiro extends Personagem{
     constructor(mana, forca, vida){
         super(mana, forca, vida)
     }
+    atacar(){
+    console.log("atacando")
+}
+
 }
