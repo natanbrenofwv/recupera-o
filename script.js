34e03d4d3154a42afb457a597b5f0d82656cdbde
NATAN BRENO FERREIRA <natan_b_ferreira@estudante.sesisenai.org.br>
2025-04-24 10:44:33 -0300

    propriedade vida privada

--- script.js
+++ script.js
@@ -1,66 +1,76 @@
 class Personagem {
+    #vida;
+
     constructor(mana, vida, forca) {
-       
-        this.mana = mana,
-        this.forca = forca,
-        this.vida = vida
-        
+        this.mana = mana;
+        this.forca = forca;
+        this.#vida = vida;
     }
-    Usarmagia(){
-        console.log("magia")
+
+    getVida() {
+        return this.#vida;
     }
+
+    setVida(novaVida) {
+        this.#vida = novaVida;
+    }
+
+    Usarmagia() {
+        console.log("magia");
+    }
+
+    atacar() {
+        console.log("ataque");
+    }
+
+    defender() {
+        console.log("defender");
+    }
+}
+
+class Mago extends Personagem {
+    constructor(mana, vida, forca) {
+        super(mana, vida, forca);
+    }
+
     
-    atacar(){
-        console.log("ataque")
-}
-    defender(){
-    console.log("defender")
-}
-
-}
-
-class mago extends Personagem{
-    constructor(mana, vida, forca){
-        super(mana, vida, forca)
-    }
-    Usarmagia(){
-        console.log("usando magia")
-    
-    }
-    atacar(){
-        console.log('bola de fogo')
-
+    Usarmagia() {
+        console.log("usando magia");
     }
 
-    defender(){
-        console.log("escudo de magia")
+    atacar() {
+        console.log("bola de fogo");
     }
-    
+
+    defender() {
+        console.log("escudo de magia");
+    }
 }
 
-class guerreiro extends Personagem{
-    constructor(mana, forca, vida){
-        super (mana, vida, forca)
+class Guerreiro extends Personagem {
+    constructor(mana, vida, forca) {
+        super(mana, vida, forca);
     }
 
-    defender(){
-        console.log ("defendendo")
+    defender() {
+        console.log("defendendo");
     }
 
-    atacar(){
-        console.log("atacar com a espada")
+    atacar() {
+        console.log("atacar com a espada");
     }
-
 }
 
-class arqueiro extends Personagem{
-    constructor(mana, forca, vida){
-        super(mana, forca, vida)
+class Arqueiro extends Personagem {
+    constructor(mana, vida, forca) {
+        super(mana, vida, forca);
     }
-    atacar(){
-    console.log("atacando")
-}
-    Usarmagia(){
-    console.log("flecha de fogo")
+
+    atacar() {
+        console.log("atacando");
+    }
+
+    Usarmagia() {
+        console.log("flecha de fogo");
     }
 }
